6db5d7191e5ab4417741b7a2c8ba76829f2db2ca
nigelarue <larue.nigel91@gmail.com>
2023-01-29 15:39:35 -0600

    ran npm init -y and installed neccesary packages. Having the same connectivity issue I had with a previous assignment with sequelize connection.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const sequelize = require("./config/connection");
+
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
